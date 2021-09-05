a5387cbbcc40e345590ab0667d43169e3558b3c6
mke2111 <roymke134@gmail.com>
2021-09-05 07:17:13 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[create index show]
+  resources :measures, only: %i[create index show]
+  resources :measurements, only: %i[create index show]
+
+  get '/signin', to: 'sessions#signin'
+  get '/error', to: 'sessions#error'
+
+  root 'measures#index'
 end
