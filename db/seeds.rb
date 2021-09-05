128d9b38689a2bbaf9efbac08c412fac23aa5ee2
mke2111 <roymke134@gmail.com>
2021-09-05 07:55:45 +0300

    Add  seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({ name: 'Rick', score: 23 })
+
+Measure.create({ name: 'waistline' })
