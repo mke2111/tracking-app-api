8f0d591438280accd63bfe2f9b0689047fae3808
mke2111 <roymke134@gmail.com>
2021-09-10 22:09:04 +0300

    Update measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,12 @@
 class Measurement < ApplicationRecord
+  validates :hours, presence: true
+  validates :measure_id, presence: true
+  validates :user_id, presence: true
   belongs_to :users
   belongs_to :measures
+
+  before_save do |m|
+    measure = m.measure
+    m.memory = measure.memory * m.hours
+  end
 end
